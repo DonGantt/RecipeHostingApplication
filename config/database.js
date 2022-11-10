3b6299f083f72d81a41e8cdbb74ac4311ee3dac4
DonGantt <ig246@yahoo.com>
2022-11-09 21:41:54 -0500

    reenabling more configs

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
-      // useNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
       // useFindAndModify: false,
       // useCreateIndex: true,
